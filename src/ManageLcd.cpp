f85fd33c38791d5d4391cff2afca88f669e2631a
gbaranski <gbaranski19@gmail.com>
2020-04-02 18:39:58 +0200

    Added LCD functionality \n-Printing HTTP on lcd doesn't error happens in ManageLcd.cpp

---
+++ src/ManageLcd.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Grzegorz Baranski on 02/04/2020.
+//
+
+#include "ManageLcd.h"
---
+++ src/ManageLcd.h
@@ -0,0 +1,14 @@
+//
+// Created by Grzegorz Baranski on 02/04/2020.
+//
+
+#ifndef ALARMCLOCK_ESP_MANAGELCD_H
+#define ALARMCLOCK_ESP_MANAGELCD_H
+
+
+class ManageLcd {
+
+};
+
+
+#endif //ALARMCLOCK_ESP_MANAGELCD_H
